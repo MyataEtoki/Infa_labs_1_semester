154446325e95cb165efdd60e65438a6d407c8d08
MyataEtoki <77798783+MyataEtoki@users.noreply.github.com>
2023-11-08 15:34:24 +0300

    Update сортировка вставками.py

--- сортировка вставками.py
+++ сортировка вставками.py
@@ -4,10 +4,5 @@ def selection(sort_nums):
         for j in range(i + 1, len(sort_nums)):
             if sort_nums[j] < sort_nums[index]:
                 index = j
-        print(sort_nums)
         sort_nums[i], sort_nums[index] = sort_nums[index], sort_nums[i]
-        print(sort_nums)
-nums = [54, 43, 3, 11, 0]
-selection(nums)
-print(nums) # Выведет [0, 3, 11, 43, 54]
 
