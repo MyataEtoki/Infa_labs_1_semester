0178eb4b61ffa00316ab9476a80909e61b0e0f07
MyataEtoki <baksyanya2006@gmail.com>
2023-11-15 02:17:17 +0300

    Придумала и сделала доп задание
    Теперь НОК и НОД выводятся оба(не только НОК) и считаются для большего количества чисел

--- least_common_multiple.py
+++ least_common_multiple.py
@@ -1,7 +1,6 @@
 def l_c_m(a,b): # НОК
     m = a * b
     return m // g_c_d(a,b) # a+b - НОД
-
 def g_c_d(a,b): #O(log(min(a,b)))
     #greatest common divisor - НОД
     while a != 0 and b != 0:
@@ -10,3 +9,8 @@ def g_c_d(a,b): #O(log(min(a,b)))
         else:
             b = b % a
     return a+b
+
+
+#НОК для 3 чисел - пояснение для кода в main файле, не используется
+def l_c_m_more(a,b,c): # НОК 3+ чисел
+    return l_c_m(l_c_m(a,b),c) # Берём НОК предыдущих двух чисел и находим его НОК с следующим числом
