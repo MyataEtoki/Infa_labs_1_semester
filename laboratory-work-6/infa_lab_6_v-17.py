86983e69b9be67a1d825634be150c787255ccf96
MyataEtoki <baksyanya2006@gmail.com>
2023-11-29 19:48:40 +0300

    обновлено

--- laboratory-work-6/infa_lab_6_v-17.py
+++ laboratory-work-6/infa_lab_6_v-17.py
@@ -1,6 +1,6 @@
-# 17. Написать программу для сравнения двух текстовых файлов и записи различий в новый текстовый файл.
+# 17. Написать программу для сравнения двух текстовых файлов и записи различий в новый текстовый файл. (сходства не смотрит)
 '''
-Считываем 1(old) и 2(new) файлы
+Считываем 1(типа old) и 2(типа new) файлы
 Идём по строкам файла -> (по символам строки?) сравниваем строку 1-го файла со строкой 2-го файла
 Если символы не равны -> записываем в 3-тий файл (old:что было в 1 файле // new: что в 2)
 Иначе: ничего не делаем
@@ -10,13 +10,13 @@
 file1, file2 = open('1.txt', 'r'), open('2.txt', 'r')
 file3 = open('3.txt', 'w')
 strings1, strings2 = [x.rstrip('\n') for x in file1.readlines()], [x.rstrip('\n') for x in file2.readlines()]
-print(strings1, strings2)
+# rstrip() - удаляет заданные символы в конце строки (по-умолч. пробелы)
 min_len = min(len(strings1), len(strings2))
 max_len = max(len(strings1), len(strings2))
 for i in range(min_len):
     if strings1[i] != strings2[i]:
         file3.write(f'string {i+1} - 1 file: {strings1[i]} // 2 file: {strings2[i]} \n')
-        # i+1 ради смотрибельности, т.к тект.ред. считает с 1, а не с 0
+        # i+1 ради смотрибельности, т.к текст.ред. считает с 1, а не с 0
 if len(strings2) > len(strings1):
     for i in range(min_len, max_len):
         file3.write(f'string {i+1} - 1 file: None // 2 file: {strings2[i]} \n')
