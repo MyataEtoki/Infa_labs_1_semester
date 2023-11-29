a43de38c6e826e0d988747b0a4d7d6785ecf50ee
MyataEtoki <baksyanya2006@gmail.com>
2023-11-29 12:04:35 +0300

    все лабы в одном репе

--- laboratory-work-4/main.py
+++ laboratory-work-4/main.py
@@ -3,8 +3,9 @@
 # находящую наименьшее общее кратное (НОК) двух чисел.
 # ДОП - можно находить НОД и НОК для более чем двух чисел.
 from least_common_multiple import l_c_m, g_c_d
-#number1, number2 = abs(int(input('Введите 1 число:'))), abs(int(input('Введите 2 число:')))
-#print(l_c_m(number1,number2))
+number1, number2 = abs(int(input('Введите 1 число:'))), abs(int(input('Введите 2 число:')))
+print(l_c_m(number1,number2))
+'''
 numbers = [int(x) for x in input('Введите числа через пробел:').split(' ')]
 gcds = [0] * (len(numbers))
 gcds[0] = numbers[0]
@@ -16,4 +17,4 @@ for i in range(len(numbers)-1):
     lcms[i+1] = l_c_m(lcms[i],numbers[i+1])
 print('НОД введённых чисел',gcds[-1])
 print('НОК введённых чисел',lcms[-1])
-
+'''
---
+++ laboratory-work-6/infa_lab_6_v-17.py
@@ -0,0 +1,23 @@
+# 17. Написать программу для сравнения двух текстовых файлов и записи различий в новый текстовый файл.
+'''
+Считываем 1(old) и 2(new) файлы
+Идём по строкам файла -> (по символам строки?) сравниваем строку 1-го файла со строкой 2-го файла
+Если символы не равны -> записываем в 3-тий файл (old:что было в 1 файле // new: что в 2)
+Иначе: ничего не делаем
+'''
+
+file1, file2 = open('1.txt', 'r'), open('2.txt', 'r')
+file3 = open('3.txt', 'w')
+strings1, strings2 = [x.rstrip('\n') for x in file1.readlines()], [x.rstrip('\n') for x in file2.readlines()]
+print(strings1, strings2)
+min_len = min(len(strings1), len(strings2))
+for i in range(min_len):
+    if strings1[i] != strings2[i]:
+        file3.write(f'string {i} - old: {strings1[i]} // new: {strings2[i]} \n')
+dif_len = max(len(strings1), len(strings2)) - min_len
+for i in range(min_len, min_len + dif_len):
+    file3.write(f'string {min_len + i} - old: None // new: {strings2[i]} \n')
+
+file1.close()
+file2.close()
+file3.close()
