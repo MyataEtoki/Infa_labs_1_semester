73db19c06b73437218d4878bda4093d74e553a9b
MyataEtoki <baksyanya2006@gmail.com>
2023-12-13 12:53:38 +0300

    upd

---
+++ laboratory-work-7/components/chats/service.py
@@ -0,0 +1,55 @@
+import utils.json_service as json_service
+
+
+def get_one_by_id(id):
+    db = json_service.get_database()
+
+    for elem in db["chats"]:
+        if elem["id"] == id:
+            return elem
+
+    return {"message": f"Элемент с {id} не найден"}
+
+
+def get_all():
+    db = json_service.get_database()
+
+    return db["chats"]
+
+
+def update_one_by_id(id, chat):
+    db = json_service.get_database()
+
+    for i, elem in enumerate(db["chats"]):
+        if elem["id"] == id:
+
+            elem["name"] = chat["name"]
+            elem["contacts"] = chat["contacts"]
+
+            json_service.set_database(db)
+            return elem
+
+    return {"message": f"Элемент с {id} не найден"}
+
+
+def delete_one_by_id(id):
+    db = json_service.get_database()
+
+    for i, elem in enumerate(db["chats"]):
+        if elem["id"] == id:
+
+            candidate = db["chats"].pop(i)
+            json_service.set_database(db)
+
+            return candidate
+
+    return {"message": f"Элемент с {id} не найден"}
+
+
+def create_one(chat):
+    db = json_service.get_database()
+
+    last_chat_id = get_all()[-1]["id"]
+    db["chats"].append({"id": last_chat_id + 1, **chat})
+
+    json_service.set_database(db)
---
+++ laboratory-work-7/components/users/service.py
@@ -0,0 +1,55 @@
+import utils.json_service as json_service
+
+
+def get_one_by_id(id):
+    db = json_service.get_database()
+
+    for elem in db["users"]:
+        if elem["id"] == id:
+            return elem
+
+    return {"message": f"Элемент с {id} не найден"}
+
+
+def get_all():
+    db = json_service.get_database()
+
+    return db["users"]
+
+
+def update_one_by_id(id, user):
+    db = json_service.get_database()
+
+    for i, elem in enumerate(db["users"]): #пронумировал юзеров (нумер, значение)
+        if elem["id"] == id: #ищем нужного юзера
+
+            elem["name"] = user["name"]
+            elem["contacts"] = user["contacts"]
+
+            json_service.set_database(db)
+            return elem
+
+    return {"message": f"Элемент с {id} не найден"}
+
+
+def delete_one_by_id(id):
+    db = json_service.get_database()
+
+    for i, elem in enumerate(db["users"]):
+        if elem["id"] == id:
+
+            candidate = db["users"].pop(i) #удаляет из массива элемент по(индексу)
+            json_service.set_database(db)
+
+            return candidate
+
+    return {"message": f"Элемент с {id} не найден"}
+
+
+def create_one(user):
+    db = json_service.get_database()
+
+    last_user_id = get_all()[-1]["id"] #значение последнего существующего айди
+    db["users"].append({"id": last_user_id + 1, **user}) #добавляем его в конец
+
+    json_service.set_database(db)
