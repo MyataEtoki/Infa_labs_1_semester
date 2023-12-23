15650a365d6745a513f037d8bbe7c375c6168cce
MyataEtoki <baksyanya2006@gmail.com>
2023-12-23 05:57:03 +0300

    я хотел написать автотесты, но сори, я спать

---
+++ laboratory-work-7/autotests.py
@@ -0,0 +1,19 @@
+import unittest
+from main import menu
+import components.chats.sevice
+import components.districts.sevice
+import components.groups.sevice
+import components.users.sevice
+
+'''
+Реализовать автотесты с использованием любого фреймворка - 2 балла
+
+Реализовать CI с этими тестами - 3 балла
+
+Использовать любую СУБД для реализации лабораторной ( важно полностью объяснить что из себя представляет ваша база данных и как она работает ) - 5 баллов 
+
+По умолчанию необходимо хранить данные в json файле
+'''
+# как мне это писать?  :(
+
+def test_
